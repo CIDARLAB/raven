4faf5e57755e55465d5c3791e922f2e946ca41c3
Jenhan Tao <jenhantao@gmail.com>
2013-05-06 14:58:11 -0400

    added logger file

---
+++ RavenCAD/src/java/Communication/RavenLogger.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Communication;
+
+/**
+ *
+ * @author Admin
+ */
+public class RavenLogger {
+    
+}
