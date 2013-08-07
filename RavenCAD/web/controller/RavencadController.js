d06bd4a5fa01932f123ac20256765cf2961f02c8
Jenhan Tao <jenhantao@gmail.com>
2013-08-07 19:28:33 -0400

    added discard button for error dialog

--- RavenCAD/web/controller/RavencadController.js
+++ RavenCAD/web/controller/RavencadController.js
@@ -428,7 +428,7 @@ $(document).ready(function() { //don't run javascript until page is loaded
                         //render image
                         $("#resultImage" + designCount).html("<img src='" + data["result"] + "'/>");
                         $('#resultImage' + designCount + ' img').wrap('<span style="width:640;height:360px;display:inline-block"></span>').css('display', 'block').parent().zoom();
-                        $('#instructionArea' + designCount).html('<div class="alert alert-danger">' + data["instructions"] + '</div>');
+                        $('#instructionArea' + designCount).html('<div>' + data["instructions"] + '</div>');
                         var status = '';
                         var saveButtons = '';
                         if (data["statistics"]["valid"] === "true") {
@@ -528,12 +528,20 @@ $(document).ready(function() { //don't run javascript until page is loaded
                     } else {
                         //display error
                         $("#designTab" + designCount).html('<div class="alert alert-danger">' +
+                                '<button class="btn" id="discardButton' + designCount + '" name="' + designCount + '">Dismiss</button><hr/>' +
                                 '<strong>Oops, an error occurred while generating your assembly plan</strong>' +
                                 '<p>Please send the following to <a href="mailto:ravencadhelp@gmail.com">ravencadhelp@gmail.com</a></p>' +
                                 '<ul><li>The error stacktrace shown below</li><li>Your input file. <small>Feel free to remove all of the sequences</small></li>' +
                                 '<li>A brief summary of what you were trying to do</li></ul>' +
                                 '<p>We appreciate your feedback. We\'re working to make your experience better</p><hr/>'
                                 + data["result"] + '</div>');
+                        $('#discardButton' + designCount).click(function() {
+                            var designNumber = $(this).attr("name");
+                            $('#designTabHeader' + designNumber).remove();
+                            $('#designTab' + designNumber).remove();
+                            $('#designTabHeader a:first').tab('show');
+                            refreshData();
+                        });
                     }
                 });
             } else {
