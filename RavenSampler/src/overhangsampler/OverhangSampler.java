6837c33d548389500e4df51f408ab5da3c12c3d4
Jenhan <jenhantao@gmail.com>
2013-11-11 18:11:04 -0800

    added geometric sampling for partitioning breaks

--- RavenSampler/src/overhangsampler/OverhangSampler.java
+++ RavenSampler/src/overhangsampler/OverhangSampler.java
@@ -22,17 +22,17 @@ public class OverhangSampler {
 
     /**
      * @param args the command line arguments
-     */
+     */  
     public static void main(String[] args) {
         RavenController controller = new RavenController();
 //        File selectedFile = new File(args[0]);
-        File selectedFile = new File("/host/Users/Admin/Dropbox/Raven_Paper/data/literature_devices/complete/collins_counters_d_p.csv");
+        File selectedFile = new File("/host/Users/Admin/Desktop/publication_dataset.csv");
         if (selectedFile != null) {
             try {
                 
                 controller.parseRavenFile(selectedFile);
                 //method, number of parts, number of runs, sample partitioning?, sample overhangs?
-                controller.run("moclo",1,1, false, true);
+                controller.run("moclo",20,10, true,false);
 //                controller.run(args[1],Integer.parseInt(args[2]),Integer.parseInt(args[3]),Boolean.parseBoolean(args[4]),Boolean.parseBoolean(args[5]));
             } catch (Exception ex) {
                 ex.printStackTrace();
@@ -40,4 +40,15 @@ public class OverhangSampler {
             }
         }
     }
+//    public static void main(String[] args) {
+//        int[] counts =new int[6];
+//        for (int i=0;i<100;i++) {
+//            int rand = SamplingPartitioning.getNumBreaks(6);
+//            counts[rand-1]=counts[rand-1]+1;
+//        }
+//        for (int i=0;i<counts.length;i++) {
+//            System.out.println(counts[i]);
+//        }
+//    }
+//    
 }
