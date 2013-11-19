c37f0fc6574d40cc3b3eb12a239be78398505591
Jenhan <jenhantao@gmail.com>
2013-11-19 07:22:31 -0800

    commented out imageDownloader

--- RavenCAD/src/java/Communication/ImageDownloader.java
+++ RavenCAD/src/java/Communication/ImageDownloader.java
@@ -18,18 +18,18 @@ public class ImageDownloader {
     
     
     public static void downloadImage(URL imageURL) throws IOException {
-        InputStream is = url.openStream();
-        OutputStream os = new FileOutputStream(destinationFile);
-
-        byte[] b = new byte[2048];
-        int length;
-
-        while ((length = is.read(b)) != -1) {
-            os.write(b, 0, length);
-        }
-
-        is.close();
-        os.close();
+//        InputStream is = url.openStream();
+//        OutputStream os = new FileOutputStream(destinationFile);
+//
+//        byte[] b = new byte[2048];
+//        int length;
+//
+//        while ((length = is.read(b)) != -1) {
+//            os.write(b, 0, length);
+//        }
+//
+//        is.close();
+//        os.close();
 }
 }
 
