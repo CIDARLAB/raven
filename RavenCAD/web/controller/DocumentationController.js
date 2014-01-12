97bc3cde87944aff240d90fe2b65c3dcb3103145
Evan Appleton <eapple@bu.edu>
2014-01-12 14:54:05 -0500

    changes to web pages

--- RavenCAD/web/controller/DocumentationController.js
+++ RavenCAD/web/controller/DocumentationController.js
@@ -37,7 +37,7 @@ $(document).ready(function() {
     }
 
     if (getCookie("authenticate") === "authenticated") {
-        $('#loginArea').html('<p class="pull-right">You are logged in as <strong>' + getCookie("user") + '</strong> <a id="logout">Log Out</a></p>');
+        $('#loginArea').html('<p class="pull-right" style="margin-top:10px">You are logged in as <strong>' + getCookie("user") + '</strong> <a id="logout">Log Out</a></p>');
         $('#logout').click(function() {
             $.get("RavenServlet", {"command": "logout"}, function() {
                 deleteCookie("authenticate");
